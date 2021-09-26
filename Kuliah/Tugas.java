e4f649761b581317f1752353e56cf7f9c0ae470b
FarhanSatria <farhansatria@telkomuniversity.ac.id>
2021-09-27 01:23:44 +0700

    Tugas.java

---
+++ Kuliah/Tugas.java
@@ -0,0 +1,8 @@
+ public class Tugas {
+ 
+	public static void main(String[] args) {
+		System.out.println("Nama: Farhan Satria Nugraha") ;
+		System.out.println("Nim: 6706213008") ;
+		System.out.println("Kelas: D3RPLA-45-02") ;
+	}
+}
